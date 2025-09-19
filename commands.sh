780b673e6d403f5e09e8d02aae88aeda95d9bf80
Prathap Boddu <beprathap@Prathaps-MacBook-Air.local>
2025-09-18 21:13:58 -0500

    initial commit

---
+++ commands.sh
@@ -0,0 +1,11 @@
+- Installation
+    pip install virtualenv
+    virtualenv venv
+    source venv/bin/activate
+    pip install dbt-snowflake
+- Create dbt profile
+    mkdir ~/.dbt
+    dbt init netflix
+    enter a number: 1
+    
+
